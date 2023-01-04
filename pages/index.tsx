b1aee5fc49575e9acd468b43222e453741117c60
hithankumar <hnandakumar.1511@gmail.com>
2023-01-04 19:46:58 +0100

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,55 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>fastandfuriouscats</title>
-        <meta name="description" content="The official page of fastandfuriouscats" />
+        <meta
+          name="description"
+          content="The official page of fastandfuriouscats"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Welcome to the official page of fastandfuriouscats
-          </p>
+          <p>Welcome to the official page of fastandfuriouscats</p>
         </div>
-        <div  className={styles.description}>
-            <p>
-                We are under development! Will be up and running soon!
-            </p>
-            <p>
-                <a title="Instagram page" href="https://www.instagram.com/fastandfuriouscats/">
+        <div className={styles.description}>
+          <p>Watch our funny animals video collection here -</p>
+          <p>
+            <a
+              title="Tik tok"
+              href="https://www.tiktok.com/@fastandfuriouscats"
+            >
+              <Image
+                src="/tiktok.svg"
+                alt="fastandfuriouscat"
+                width={100}
+                height={100}
+                priority
+              />
+            </a>
+          </p>
+          <p>
+            <a
+              title="Youtube channel"
+              href="https://www.youtube.com/@fastandfuriouscats"
+            >
+              <Image
+                src="/youtube.svg"
+                alt="fastandfuriouscat"
+                width={100}
+                height={100}
+                priority
+              />
+            </a>
+
+            {/* <a title="Instagram page" href="https://www.instagram.com/fastandfuriouscats/">
                     <Image
                         src="/insta.svg"
                         alt="fastandfuriouscat"
@@ -31,8 +57,8 @@ export default function Home() {
                         height={50}
                         priority
                     />
-                </a> 
-            </p>
+                </a>  */}
+          </p>
         </div>
 
         <div className={styles.center}>
@@ -46,8 +72,7 @@ export default function Home() {
             />
           </div>
         </div>
-
       </main>
     </>
-  )
+  );
 }
